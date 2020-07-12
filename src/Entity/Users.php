1134fc6b1c529a83c3c05d95f21d5cc8a4452462
Habib Kanbar <67437103+habibdevsy@users.noreply.github.com>
2020-07-12 23:35:29 +0300

    Update Users.php

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -3,62 +3,47 @@
 namespace App\Entity;
 
 use App\Repository\UsersRepository;
+    
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UsersRepository::class)
  */
-class Users
+class Users implements UserInterface
+
 {
+
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Groups("api")
      */
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=100)
-     */
-    private $userName;
-
-    /**
-     * @ORM\Column(type="string", length=100)
-     */
-    private $password;
-
-    /**
-     * @ORM\Column(type="string", length=200)
+     * @ORM\Column(type="string", length=180, unique=true)
+     * @Groups("api")
      */
     private $email;
+
+    /**
+     * @ORM\Column(type="json")
+     * @Groups("api")
+     */
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
 
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getUserName(): ?string
-    {
-        return $this->userName;
-    }
-
-    public function setUserName(string $userName): self
-    {
-        $this->userName = $userName;
-
-        return $this;
-    }
-
-    public function getPassword(): ?string
-    {
-        return $this->password;
-    }
-
-    public function setPassword(string $password): self
-    {
-        $this->password = $password;
-
-        return $this;
     }
 
     public function getEmail(): ?string
@@ -72,7 +57,26 @@ class Users
 
         return $this;
     }
-     public function getRoles(): array
+    public function setUserName(string $userName): self
+    {
+        $this->userName = $userName;
+
+        return $this;
+    }
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserName(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
     {
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
@@ -87,4 +91,37 @@ class Users
 
         return $this;
     }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
 }
